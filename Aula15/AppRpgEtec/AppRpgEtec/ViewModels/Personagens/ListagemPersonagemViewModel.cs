7fbf7c5ba8dfe831fbad0b33acd6e6264b7d6aee
RogerioZFaga <rogeriozfaga@gmail.com>
2025-06-23 21:33:26 -0300

    fixed

--- Aula15/AppRpgEtec/AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
+++ Aula15/AppRpgEtec/AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -22,8 +22,11 @@ namespace AppRpgEtec.ViewModels.Personagens
             _ = ObterPersonagens();
 
             NovoPersonagemCommand = new Command(async () => { await ExibirCadastroPersonagem(); });
+            RemoverPersonagemCommand = new Command<Personagem>(async (Personagem p) => { await RemoverPersonagem(p); });
         }
         public ICommand NovoPersonagemCommand { get; }
+
+        public ICommand RemoverPersonagemCommand { get; set;}
         public async Task ObterPersonagens()
         {
             try //Junto com o Cacth evitará que erros fechem o aplicativo
@@ -66,5 +69,25 @@ namespace AppRpgEtec.ViewModels.Personagens
                 }
             }
         }
+
+        public async Task RemoverPersonagem(Personagem p)
+        {
+            try
+            {
+                if (await Application.Current.MainPage.DisplayAlert("Confirmação", $"Confirma a remoção de {p.Nome}?", "Sim", "Não"))
+                {
+                    await pService.DeletePersonagemAsync(p.Id);
+
+                    await Application.Current.MainPage.DisplayAlert("Mensagem", "Personagem removido com sucesso!", "Ok");
+
+                    _ = ObterPersonagens();
+                }
+            }
+            catch(Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Ops", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
     }
 }
